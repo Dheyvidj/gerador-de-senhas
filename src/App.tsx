dcaaf9b30c3a2d433c18a1879a11fed27a18e151
dheyvid-lima <78829087+Dheyvidj@users.noreply.github.com>
2023-01-30 09:03:01 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+
+  return (
+    <div className="flex justify-center items-center w-screen h-screen ">
+      <div className="">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
